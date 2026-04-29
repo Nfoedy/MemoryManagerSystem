be9bdf6f0bf59ab43f19e10b55768440b206c0a7
RiccardoConforti <156352123+Nfoedy@users.noreply.github.com>
2026-04-29 13:52:33 +0200

    add New & Delete

--- include/MemoryManager/MemoryManager.h
+++ include/MemoryManager/MemoryManager.h
@@ -10,6 +10,43 @@ namespace MM
 
     void PrintStats();  // Stampa le statistiche base sull'utilizzo della memoria
     void DumpLeaks();  // Stampa  perdite di memoria
+
+    // NEW
+    template<typename T, typename... Args>
+    T* New(Args&&... args)    // Alloca memoria per un oggetto di tipo T e lo costruisce con i parametri passati
+    {
+        // 1. Allocazione memoria
+        void* mem = MM::Malloc(sizeof(T)); // Alloca memoria per un oggetto di tipo T
+
+        if(!mem)
+        {
+            return nullptr; // Se l'allocazione fallisce, restituisce nullptr
+        }
+
+        // 2. Costruzione oggetto
+        return new(mem) T(std::forward<Args>(args)...); // Costruisce l'oggetto di tipo T nella memoria allocata usando il costruttore con i parametri passati
+    }
+
+
+    // DELETE
+    template<typename T>
+    void Delete(T* ptr)      // Distrugge un oggetto di tipo T e libera la memoria associata
+    {
+        if(!ptr)
+        {
+            return; // Se il puntatore è nullptr, non fa nulla
+        }
+
+        // 1. Distruzione oggetto
+        ptr->~T(); // Chiama il distruttore dell'oggetto di tipo T
+
+        // 2. Liberazione memoria
+        MM:Free(ptr); // Libera la memoria associata all'oggetto usando la funzione Free del MemoryManager
+    }
+
+
+
+
 }
 
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,34 @@
 #include "MemoryManager/MemoryManager.h"
+#include <iostream>
+
+
+class Player
+{
+public:
+    Player(int hp)
+    {
+        std::cout << "Player creato con hp: " << hp << std::endl;
+    }
+
+    ~Player()
+    {
+        std::cout << "Player distrutto" << std::endl;
+    }
+};
+
 
 
 int main()
 {
-    void* ptr1 = MM::Malloc(64);    // Alloca 64 byte di memoria e ottiene un puntatore ad essa
-    void* ptr2 = MM::Malloc(128);
+    //void* ptr1 = MM::Malloc(64);    // Alloca 64 byte di memoria e ottiene un puntatore ad essa
+    //void* ptr2 = MM::Malloc(128);
 
+    //MM::Free(ptr1);    // Libera la memoria allocata in precedenza
+    //MM::Free(ptr2);    // Libera la memoria allocata in precedenza
 
-    MM::Free(ptr1);    // Libera la memoria allocata in precedenza
-    MM::Free(ptr2);    // Libera la memoria allocata in precedenza
+    Player* p1 = MM::New<Player>(100); // Alloca memoria per un oggetto Player e lo costruisce con hp = 100
+
+    MM::Delete(p1); // Distrugge l'oggetto Player e libera la memoria associata
 
     MM::PrintStats();
     MM::DumpLeaks();
