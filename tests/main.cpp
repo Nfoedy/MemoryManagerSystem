d3d8d6b06206814ac2d2cfb9d4edbb013a048bf7
RiccardoConforti <riccardoconforti96@gmail.com>
2026-05-14 20:42:00 +0200

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,75 +4,43 @@
 
 
 // TEST CLASS
-
-class Player
-{
-private:
-    int m_Hp;
-
-public: 
-    Player(int hp) : m_Hp(hp)
+/*
+    class Player
     {
-        std::cout << "[Player] Creato con HP: " << m_Hp << std::endl;
-    }
+    private:
+        int m_Hp;
 
-    ~Player()
-    {
-        std::cout << "[Player] Distrutto" << std::endl;
-    }
+    public: 
+        Player(int hp) : m_Hp(hp)
+        {
+            std::cout << "[Player] Creato con HP: " << m_Hp << std::endl;
+        }
 
-};
+        ~Player()
+        {
+            std::cout << "[Player] Distrutto" << std::endl;
+        }
 
-
-// TEST 1 - NO LEAK
-
-void Test_NoLeaks()
-{
-    std::cout << " ===== TEST NO LEAKS =====" << std::endl;
-
-    // Allocazione raw
-    void* a = MM_MALLOC(32);
-
-    // Allocazione oggetto
-    Player* p = MM_NEW(Player,100);
-
-    // Deallocazione
-    MM_FREE(a);
-    MM_DELETE(p);
-
-    MM::PrintStats();
-    MM::DumpLeaks();
-}
+    };
+*/
 
 
-// TEST 2 - LEAK
 
 
-void Test_Leaks()
-{
-    std::cout << " ===== TEST LEAKS =====" << std::endl;
+int main(){
 
-    // Allozacione raw
-    int* a = (int*)MM_MALLOC(4);
-
-    // Allocazione oggetto
-    Player* p = MM_NEW(Player,100);
+    std::cout << "\n =======TEST=========" << std::endl;
     
-    //Deallocazione (ne libero solo 1)
-    MM_FREE(a);
+    void* small = MM_MALLOC(32);   //Small obj
+    void* big = MM_MALLOC(128);    // General
+
+    MM_FREE(small);
+    
 
     MM::PrintStats();
     MM::DumpLeaks();
-}
 
-
-
-
-int main()
-{
-    Test_NoLeaks();
-
-    Test_Leaks();
 
     return 0;
+
 }
